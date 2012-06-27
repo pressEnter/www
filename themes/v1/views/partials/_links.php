718227cc328614988d864d8d7045550c5551953d
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-06-27 02:12:47 -0300

    Actualiadas urls de sitios sociales

--- themes/v1/views/partials/_links.php
+++ themes/v1/views/partials/_links.php
@@ -30,12 +30,12 @@
 				<?php $images_dir = Yii::app()->theme->baseUrl ."/images"; ?>
 				<ul>
 					<li>
-						<a href="#" target="_blank">
+						<a href="https://twitter.com/#!/PressEnterComAr" target="_blank">
 							<img src="<?php echo $images_dir; ?>/icons/twitter-logo-square-24x24.png" alt="" title="" />
 						</a>
 					</li>
 					<li>
-						<a href="#" target="_blank">
+						<a href="http://www.facebook.com/pages/PressEnter-Soluciones-Inform%C3%A1ticas/105554509534761" target="_blank">
 							<img src="<?php echo $images_dir; ?>/icons/facebook-logo-square-24x24.png" alt="" title="" />
 						</a>
 					</li>
