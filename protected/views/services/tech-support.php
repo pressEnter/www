e50c8bb6ba8a5070dba420e31de31f943dd13eb2
Fernando Toledo <ftoledo@docksud.com.ar>
2012-06-29 14:19:48 -0300

    prueba de textos para servicios

---
+++ protected/views/services/tech-support.php
@@ -0,0 +1,8 @@
+<h1>Soporte Técnico</h1>
+
+<p>Nuestro servicio de asistencia técnica (LEVEL1/LEVEL4) proprociona a la empresas la robustez y seguridad necesaria para que su negocio
+funcione sin interrupciones los 365 días del año. </p>
+
+<p>Utilizamos las mejores herramientas tenologicas para realizar mantenimientos preventivos y correctivos resolviendo cualquier inconveniente
+que surga sobre la infraestructura que dispone su empresa.</p>
+
