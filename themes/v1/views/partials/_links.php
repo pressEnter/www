4960e8ba34655f336ad10bdb96c873a3a523f86f
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-07-04 01:40:07 -0300

    Arreglados links del footer

--- themes/v1/views/partials/_links.php
+++ themes/v1/views/partials/_links.php
@@ -1,17 +1,17 @@
 			<ul>
 				<li><h4>Capacitación</h4></li>
-				<li><?php echo CHtml::link('Symfony', 'training/symfony'); ?></li>
-				<li><?php echo CHtml::link('Drupal', 'training/drupal'); ?></li>
-				<li><?php echo CHtml::link('Python/DJango', 'training/python-django'); ?></li>
-				<li><?php echo CHtml::link('XHTML/CSS', 'training/xhtml-css'); ?></li>
-				<li><?php echo CHtml::link('GNU/Linux', 'training/gnu-linux'); ?></li>
+				<li><?php echo CHtml::link('Symfony', '/training/symfony'); ?></li>
+				<li><?php echo CHtml::link('Drupal', '/training/drupal'); ?></li>
+				<li><?php echo CHtml::link('Python/DJango', '/training/python-django'); ?></li>
+				<li><?php echo CHtml::link('XHTML/CSS', '/training/xhtml-css'); ?></li>
+				<li><?php echo CHtml::link('GNU/Linux', '/training/gnu-linux'); ?></li>
 			</ul>
 			<ul>
 				<li><h4>Servicios</h4></li>
-				<li><?php echo CHtml::link('Desarollos de apps', 'services/web-development'); ?></li>
-				<li><?php echo CHtml::link('SUGAR Activities', 'services/sugar-activities'); ?></li>
-				<li><?php echo CHtml::link('Soporte Técnico', 'services/tech-support'); ?></li>
-				<li><?php echo CHtml::link('Hosting', 'services/hosting'); ?></li>
+				<li><?php echo CHtml::link('Desarollos de apps', '/services/web-development'); ?></li>
+				<li><?php echo CHtml::link('SUGAR Activities', '/services/sugar-activities'); ?></li>
+				<li><?php echo CHtml::link('Soporte Técnico', '/services/tech-support'); ?></li>
+				<li><?php echo CHtml::link('Hosting', '/services/hosting'); ?></li>
 			</ul>
 			<div id="social-links">
 				<?php $images_dir = Yii::app()->theme->baseUrl ."/images"; ?>
