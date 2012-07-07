bee680a9bf0e89ecc6106ecd51d5bb91a0218649
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-07-07 16:57:20 -0300

    Avances en cosas

--- protected/views/training/instructors/_miguel_garcia.php
+++ protected/views/training/instructors/_miguel_garcia.php
@@ -1,10 +1,13 @@
 <div class="details instructor-details">
 	<div class="hgroup">
 		<h3>Acerca del instructor</h3>
-		<h4><?php echo CHtml::link('Miguel García', array('/about-us', '#' => 'instructor-miguel-garcia')); ?></h4>
+		<h4><?php echo CHtml::link('Miguel García', array('/about-us', '#' => 'miguel-garcia')); ?></h4>
 	</div>
 	<div class="figure">
-		<?php echo CHtml::image(Yii::app()->request->getBaseUrl(true).'/images/training/no-existent-image.png', 'Instructor', array()); ?>
+		<?php echo CHtml::link(
+			CHtml::image(Yii::app()->request->getBaseUrl(true).'/images/about-us/miguel-garcia-100x100-color.jpg', 'Miguel García', array('title' => 'Miguel García')),
+			array('/about-us', '#' => 'miguel-garcia')
+			); ?>
 	</div>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris tellus, convallis id placerat quis, viverra id neque. Vestibulum malesuada pellentesque magna eu fringilla. Sed non rhoncus libero. Integer ullamcorper felis non quam malesuada interdum. Etiam vel mauris nec felis commodo egestas. Donec ultricies mauris vitae lectus condimentum accumsan. Phasellus pharetra ipsum quis magna posuere et laoreet orci aliquam. Donec laoreet feugiat sapien eu luctus. Fusce ac faucibus magna. Morbi sed erat id velit pharetra rhoncus blandit eu dui. Nulla facilisi. Donec in mi eget leo ullamcorper pharetra. Aliquam id turpis pretium dui lacinia suscipit. Nullam nunc mi, posuere id vehicula vitae, imperdiet vel velit.</p>
 </div>
