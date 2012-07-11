df03885509f14a35599ebf71b4abcc728fdfb46f
Fernando Toledo <ftoledo@docksud.com.ar>
2012-07-11 14:29:55 -0300

    correcion de links usar array() para que tome el url desde el ruteo, sino genera una url absoluta

--- themes/v1/views/partials/_links.php
+++ themes/v1/views/partials/_links.php
@@ -1,17 +1,17 @@
 			<ul>
 				<li><h4>Capacitación</h4></li>
-				<li><?php echo CHtml::link('Symfony', '/training/symfony'); ?></li>
-				<li><?php echo CHtml::link('Drupal', '/training/drupal'); ?></li>
-				<li><?php echo CHtml::link('Python/DJango', '/training/python-django'); ?></li>
-				<li><?php echo CHtml::link('XHTML/CSS', '/training/xhtml-css'); ?></li>
-				<li><?php echo CHtml::link('GNU/Linux', '/training/gnu-linux'); ?></li>
+				<li><?php echo CHtml::link('Symfony', array('/training/symfony')); ?></li>
+				<li><?php echo CHtml::link('Drupal', array('/training/drupal')); ?></li>
+				<li><?php echo CHtml::link('Python/DJango', array('/training/python-django')); ?></li>
+				<li><?php echo CHtml::link('XHTML/CSS', array('/training/xhtml-css')); ?></li>
+				<li><?php echo CHtml::link('GNU/Linux', array('/training/gnu-linux')); ?></li>
 			</ul>
 			<ul>
 				<li><h4>Servicios</h4></li>
-				<li><?php echo CHtml::link('Desarollos de apps', '/services/web-development'); ?></li>
-				<li><?php echo CHtml::link('SUGAR Activities', '/services/sugar-activities'); ?></li>
-				<li><?php echo CHtml::link('Soporte Técnico', '/services/tech-support'); ?></li>
-				<li><?php echo CHtml::link('Hosting', '/services/hosting'); ?></li>
+				<li><?php echo CHtml::link('Desarollos de apps', array('/services/web-development')); ?></li>
+				<li><?php echo CHtml::link('SUGAR Activities', array('/services/sugar-activities')); ?></li>
+				<li><?php echo CHtml::link('Soporte Técnico', array('/services/tech-support')); ?></li>
+				<li><?php echo CHtml::link('Hosting', array('/services/hosting')); ?></li>
 			</ul>
 			<div id="social-links">
 				<?php $images_dir = Yii::app()->theme->baseUrl ."/images"; ?>
