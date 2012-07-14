e6bc84ff61afb61f0e4353869c7a13b477ee3a24
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-07-14 16:35:05 -0300

    Stuff

--- protected/views/site/pages/about-us.php
+++ protected/views/site/pages/about-us.php
@@ -24,7 +24,7 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/jqu
 					CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/infocus-linkedin-logo-square2-24x24.png', 'Ver perfil de Héctor Sánchez en Linkedin', array('title' => 'Ver perfil de Héctor Sánchez en Linkedin')),
 					'http://www.linkedin.com/in/karucha', array('target' => '_blank')
                 );
-				echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/twitter-bird.png', 'Cuenta de Twitter', array('title' => 'Cuenta de Twitter')),'https://twitter.com/hectorksanchez', array('target' => '_blank'));?>
+				echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/twitter-bird-24x24.png', 'Cuenta de Twitter', array('title' => 'Cuenta de Twitter')),'https://twitter.com/hectorksanchez', array('target' => '_blank'));?>
 				</div>
 			</div>
 		</div>
@@ -86,7 +86,7 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/jqu
 						'http://www.facebook.com/luciano.baraglia', array('target' => '_blank')
 					); 
 					echo CHtml::link(
-						CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/infocus-twitter-logo-square-24x24.png', 'Cuenta de Twitter', array('title' => 'Cuenta de Twitter')),
+						CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/twitter-bird-24x24.png', 'Cuenta de Twitter', array('title' => 'Cuenta de Twitter')),
 						'https://twitter.com/lucianobaraglia', array('target' => '_blank')
 					);
 					echo CHtml::image(Yii::app()->theme->baseUrl.'/images/icons/infocus-gmail-24x24.png', 'GTalk: luciano.baraglia@gmail.com', array('class' => 'gtalk-info', 'title' => 'GTalk: luciano.baraglia@gmail.com'));
