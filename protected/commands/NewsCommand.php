c7b78db4bc53d4bb00e12d36dd393e26ebdc7873
Fernando Toledo <ftoledo@docksud.com.ar>
2012-07-15 04:02:50 -0300

    actions para las news

--- protected/commands/NewsCommand.php
+++ protected/commands/NewsCommand.php
@@ -3,15 +3,17 @@
 class NewsCommand extends CConsoleCommand {
 
   public function actionIndex() {
-    printf ("\n == Administrador de noticias ==\n\n");
+    printf("\n == Administrador de noticias ==\n\n");
     echo $this->getHelp();
   }
+
   public function actionList() {
+    ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
     $posts = Post::model()->findAll();
     printf("\n");
     if (count($posts) > 0) {
       foreach ($posts as $post) {
-        printf("|%d|%s|%s|%s\n", $post->id, $post->published_date,$post->category->description ,$post->title);
+        printf("|%d|%s|%s|%s\n", $post->id, date("d/m/Y", strtotime($post->published_date)), $post->category->description, $post->title);
       }
     } else {
       printf("No hay ningun post.\n");
@@ -20,12 +22,16 @@ class NewsCommand extends CConsoleCommand {
   }
 
   public function actionAdd() {
-    $tz = new DateTimeZone('America/Argentina/Buenos_Aires');
+    ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
 
     $fecha = $this->prompt("Fecha (Enter: fecha de actual)?");
     if ($fecha == '') {
-      $fecha = new DateTime('now', $tz);
+      $fecha = 'now';
+    } else {
+      list ($d, $m, $a) = explode('-', str_replace('/', '-', $fecha));
+      $fecha = "$a-$m-$d";
     }
+    $fecha = new DateTime($fecha);
     $categorias = PostCategory::model()->findAll();
     printf("\nCategorias: \n");
     foreach ($categorias as $categoria) {
@@ -50,7 +56,7 @@ class NewsCommand extends CConsoleCommand {
       $post->category_id = $cat;
       $post->published_date = $fecha->format('Y:m:d H:i:s');
       $post->body = file_get_contents($tmp_name);
-      $post->slug = "pepe";
+      $post->slug = $title;
       $post->save();
       printf("OK\n");
     }
@@ -69,10 +75,32 @@ class NewsCommand extends CConsoleCommand {
   }
 
   public function actionEdit($id) {
+    ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
     $post = Post::model()->findByPk($id);
     if ($post) {
+      $fecha = $this->prompt("Fecha: " . date('d/m/Y', strtotime($post->published_date)) . " (Enter: deja fecha actual)?");
+      if ($fecha == '') {
+        $fecha = 'now';
+      } else {
+        list ($d, $m, $a) = explode('-', str_replace('/', '-', $fecha));
+        $fecha = "$a-$m-$d";
+      }
+      $fecha = new DateTime($fecha);
+      $categorias = PostCategory::model()->findAll();
+      printf("\nCategorias: \n");
+      foreach ($categorias as $categoria) {
+        printf("[%s] %d - %s\n", $post->category_id == $categoria->id ? 'X' : ' ', $categoria->id, $categoria->description);
+      }
+      $cat = $this->prompt("Categoria (Enter deja actual)? ");
+      if ($cat == '') {
+        $cat = $post->category_id;
+      }
+      printf("Titulo: %s\n", $post->title);
+      $title = $this->prompt("(Enter deja actual)?");
+      if ($title == '') {
+        $title = $post->title;
+      }
       $tmp_name = tempnam(sys_get_temp_dir(), 'news');
-
       file_put_contents($tmp_name, $post->body);
       $mtime = filectime($tmp_name);
       clearstatcache();
@@ -83,12 +111,14 @@ class NewsCommand extends CConsoleCommand {
       } else {
         printf("Archivo modificado. Guardando los cambios.\n");
         $post->body = file_get_contents($tmp_name);
+        $post->published_date = $fecha->format('Y-m-d H:i:s');
+        $post->title = $title;
+        $post->category_id = $cat;
         $post->save();
         printf("OK\n");
       }
       unlink($tmp_name);
-    }
-    else {
+    } else {
       printf("No existe el post.\n");
     }
   }
