52fc6c68271ce8bf91413adb20210afc4e842e85
Fernando Toledo <ftoledo@tom.pressenter.com.ar>
2012-07-15 05:46:56 -0300

    schema para sqlite

--- protected/data/schema.sqlite.sql
+++ protected/data/schema.sqlite.sql
@@ -1,28 +1,33 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-);
+BEGIN;
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test4', 'pass4', 'test4@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test5', 'pass5', 'test5@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test6', 'pass6', 'test6@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test7', 'pass7', 'test7@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test8', 'pass8', 'test8@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test9', 'pass9', 'test9@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test10', 'pass10', 'test10@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test11', 'pass11', 'test11@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test12', 'pass12', 'test12@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test13', 'pass13', 'test13@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test14', 'pass14', 'test14@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test15', 'pass15', 'test15@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test16', 'pass16', 'test16@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test17', 'pass17', 'test17@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test18', 'pass18', 'test18@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test19', 'pass19', 'test19@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test20', 'pass20', 'test20@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test21', 'pass21', 'test21@example.com');
+CREATE TABLE deltas (
+	id INTEGER NOT NULL PRIMARY KEY autoincrement,
+	number INTEGER NOT NULL UNIQUE,
+	tag VARCHAR(20) NOT NULL,
+	description VARCHAR(255)
+) ;
+
+CREATE TABLE post_categories (
+	id INTEGER NOT NULL PRIMARY KEY autoincrement,
+	description VARCHAR(99) NOT NULL,
+	slug VARCHAR(99) NOT NULL UNIQUE
+) ;
+
+CREATE TABLE posts (
+	id INTEGER NOT NULL PRIMARY KEY autoincrement,
+	published_date date,
+	category_id INT,
+	title VARCHAR(99) NOT NULL,
+	slug VARCHAR(255) NOT NULL UNIQUE,
+	body TEXT NOT NULL,
+	published BOOLEAN NOT NULL DEFAULT 1,
+	author_id INT
+) ;
+
+CREATE TABLE post_images (
+	id INTEGER NOT NULL PRIMARY KEY autoincrement,
+	post_id INT,
+	url VARCHAR(255) NOT NULL
+) ;
+
+COMMIT;
