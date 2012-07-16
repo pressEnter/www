b3a3e9d5f9382ec452e36e041a31adb4d79ab489
Fernando Toledo <ftoledo@docksud.com.ar>
2012-07-16 14:55:24 -0300

    partials

--- themes/v1/views/layouts/main.php
+++ themes/v1/views/layouts/main.php
@@ -135,9 +135,6 @@
 		</div><!-- footer -->
 
 	</div><!-- wrapper -->
-  <?php print_R($_SERVER['REMOTE_ADDR']); ?>
-  <?php if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))): ?>
-  <!-- Piwik --><script type="text/javascript">var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.tom.pressenter.com.ar/" : "http://piwik.tom.pressenter.com.ar/");document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));</script><script type="text/javascript">try {var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);piwikTracker.trackPageView();piwikTracker.enableLinkTracking();} catch( err ) {}</script><noscript><p><img src="http://piwik.tom.pressenter.com.ar/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript><!-- End Piwik Tracking Code -->
-  <?php endif;?>
+  <?php $this->renderPartial('webroot.themes.v1.views.partials._piwik');?>
   </body>
 </html>
