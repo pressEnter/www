b777dddaf06bd3d89466319b24f71812860ccd49
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-07-16 15:56:04 -0300

    Modificación primer noticia y corrección  de social links

--- protected/data/deltas/002.sql
+++ protected/data/deltas/002.sql
@@ -11,12 +11,17 @@ INSERT INTO post_categories (id, description, slug) VALUES
 (8, 'Sitio web', 'sitio-web')
 ;
 
-INSERT INTO posts (published_date, title, slug, body, published) VALUES 
-('2012-07-16', 'Rediseño del sitio Web', 'rediseno-del-sitio-web', 
+INSERT INTO posts (id, published_date, title, slug, body, published) VALUES 
+(1, '2012-07-16', 'Rediseño del sitio Web de pressEnter', 'rediseno-del-sitio-web-de-pressenter', 
 '<p>Luego de bastante trabajo, estamos felices de anunciar el relanzamiento oficial de la imagen de <strong>pressEnter</strong>.</p>
 <p>Ya habíamos iniciado el proceso hace un tiempo con la decisión de cambiar el logo, algo que siempre suele dar temor.</p>
 <p>Ahora, con la renovación de nuestro sitio web después de varios años, buscamos estar a la altura de este momento que están viviendo las <strong>cooperativas de desarrollo de software</strong>.</p>
-<p>Estaremos encantados de recibir comentarios, recomendaciones y/o críticas constructivas acerca de nuestra nueva imagen.</p>',
+<p>En cuanto a las tecnologías usadas para desarrollarlo, nos decidimos por utilizar <a href="http://yiiframework.com" target="_blank">Yii Framework</a> como base porque nos resulta liviano, fácil de usar y extender. La alternativa era utilizar <a href="http://drupal.org" target="_blank">Drupal</a>, pero no nos daba el nivel de control que queríamos tener sobre el desarrillo del sitio.</p>
+<p>Como librería javascript utilizamos <a href="http://jquery.com" target="_blank">jQuery</a> y <a href="http://jquerytools.org" target="_blank">jQuerytools</a> para los efectos.</p>
+<p>La gráfica es propia, aunque utilizamos íconos de <a href="http://dryicons.com" target="_blank">Dryicons</a>, <a href="http://www.elegantthemes.com/" target="_blank">Nicholas Roach</a>, <a href="http://delacro.deviantart.com/" target="_blank">Delacro</a> y <a href="http://mysitemyway.com/" target="_blank">Mysitemyway</a>.</p>
+<p>Para el mapa, utilizamos <a href="http://www.openstreetmap.org" target="_blank">OpenStreetMap</a>.</p>
+<p>Prestamos especial atención a que no haya ni una sóla página dentro del sitio que no tenga código <a href="http://validator.w3.org/check?uri=referer" target="_blank">XHTML válido</a>, al igual que el <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" target="_blank">CSS</a>.</p>
+<p style="margin-top: 10px;">En fin...estaremos encantados de recibir comentarios, recomendaciones y/o críticas constructivas acerca de nuestra nueva imagen.</p>',
 1)
 ;
 
--- protected/views/partials/_social_links.php
+++ protected/views/partials/_social_links.php
@@ -6,14 +6,13 @@
 				scrolling="no" 
 				frameborder="0" 
 				style="border:none; overflow:hidden; width:450px; height:21px;" 
-				allowTransparency="true">
+				>
 			</iframe>
 		</li>
 		<li class="share-tt">
 			<iframe 
 				scrolling="no" 
 				frameborder="0" 
-				allowtransparency="true" 
 				src="https://platform.twitter.com/widgets/tweet_button.1340179658.html#_=1340754417216&amp;count=horizontal&amp;id=twitter-widget-40&amp;lang=es&amp;original_referer=https%3A%2F%2Ftwitter.com%2Fabout%2Fresources%2Fbuttons%23tweet&amp;size=m&amp;text=<?php echo rawurlencode($title); ?>&amp;url=<?php echo rawurlencode($url); ?>&amp;via=PressEnterComAr" 
 				class="twitter-share-button twitter-count-horizontal" 
 				style="width: 117px; height: 20px;" 
