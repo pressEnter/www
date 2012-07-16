d5b45e028b968fa13a7448265be7d28f1dc451ba
Miguel Garcia <miguel.garcia@gmail.com>
2012-07-16 20:01:24 -0300

    errores ORTOgraficos

--- protected/views/services/tech-support.php
+++ protected/views/services/tech-support.php
@@ -4,7 +4,7 @@
         <h1>Soporte Técnico Linux</h1>
     </div>
 	<div class="body service-details">
-		<p> Nuestro servicio de asistencia técnica profesional, proprociona a la empresas la robustez y seguridad necesaria para que su negocio funcione sin interrupciones los 365 días del año. </p>
-		<p>Utilizamos las mejores herramientas tenologicas para realizar mantenimientos preventivos y correctivos resolviendo cualquier inconveniente que surga sobre la infraestructura que dispone su empresa.</p>
+		<p> Nuestro servicio de asistencia técnica profesional, proporciona a la empresas la robustez y seguridad necesaria para que su negocio funcione sin interrupciones los 365 días del año. </p>
+		<p>Utilizamos las mejores herramientas tecnológicas para realizar mantenimientos preventivos y correctivos resolviendo cualquier inconveniente que surja sobre la infraestructura que dispone su empresa.</p>
 	</div>
 </div>
