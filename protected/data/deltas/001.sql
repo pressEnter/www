6cddc8c99011f49b748beb8928301e93d2976046
Luciano Baraglia <luciano.baraglia@gmail.com>
2012-10-22 15:57:15 -0300

    Preparando ando

--- protected/data/deltas/001.sql
+++ protected/data/deltas/001.sql
@@ -1,9 +1,6 @@
 BEGIN;
 
-INSERT INTO users (username, password, email) VALUES ('lbaraglia', SHA1('Asps97huMroe'), 'lbaraglia@pressenter.com.ar');
-INSERT INTO users (username, password, email) VALUES ('ftoledo', SHA1('gyBe48saL74'), 'ftoledo@pressenter.com.ar');
-INSERT INTO users (username, password, email) VALUES ('mgarcia', SHA1('tan14LOrds'), 'mgarcia@pressenter.com.ar');
-INSERT INTO users (username, password, email) VALUES ('hsanchez', SHA1('Fugal45deL'), 'hsanchez@pressenter.com.ar');
+INSERT INTO users (username, password, email) VALUES ('admin', SHA1('admin'), 'admin@example');
 
 INSERT INTO deltas (number, tag, description) VALUES ('001', 'datos', 'Usuarios.');
 
